2ac93a2c1a0e7d257fd892fbfc9db6943ff53391
liam <liam_devlin@live.co.uk>
2021-09-04 22:41:51 +0100

    started but got nowhere

---
+++ bgfx-shader-tool/bgfx-shader-tool/BGFXShaderToolHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bgfx_shader_tool
+{
+    public class BGFXShaderToolHelpers
+    {
+        public const string SHADER_C_PATH = "res/";
+
+
+    }
+}
